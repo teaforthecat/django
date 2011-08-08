fee1b698c0d736f760274472ce9707f6c5b6479d
Chris Thompson <chris.thompson@walkerart.org>
2011-08-08 10:46:11 -0500

    in create_test_db, allow south to create all tables(through syncdb) with the migrate_all option

--- django/db/backends/creation.py
+++ django/db/backends/creation.py
@@ -232,11 +232,19 @@ class BaseDatabaseCreation(object):
         # Report syncdb messages at one level lower than that requested.
         # This ensures we don't get flooded with messages during testing
         # (unless you really ask to be flooded)
-        call_command('syncdb',
-            verbosity=max(verbosity - 1, 0),
-            interactive=False,
-            database=self.connection.alias,
-            load_initial_data=False)
+        if 'south' in settings.INSTALLED_APPS:
+            call_command('syncdb',
+                         verbosity=max(verbosity - 1, 0),
+                         migrate_all=True,         
+                         interactive=False,
+                         database=self.connection.alias,
+                         load_initial_data=False)
+        else:
+            call_command('syncdb',
+                         verbosity=max(verbosity - 1, 0),
+                         interactive=False,
+                         database=self.connection.alias,
+                         load_initial_data=False)
 
         # We need to then do a flush to ensure that any data installed by
         # custom SQL has been removed. The only test data should come from
